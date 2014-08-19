5362893f2e0a7d4be4f3387dc3f60ec0e179facd
Iván Mervich <imervich@gmail.com>
2014-08-19 14:50:55 -0500

    Added FacebookLogin class

---
+++ Instagram/FacebookLoginViewController.m
@@ -0,0 +1,23 @@
+//
+//  FacebookLoginViewController.m
+//  Instagram
+//
+//  Created by Iván Mervich on 8/19/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import "FacebookLoginViewController.h"
+
+@interface FacebookLoginViewController ()
+
+@end
+
+@implementation FacebookLoginViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+@end
