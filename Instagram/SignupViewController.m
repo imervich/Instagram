67179ade5bcd77745e98c3c6559de01b0793a70e
Iván Mervich <imervich@gmail.com>
2014-08-19 14:45:09 -0500

    Added class for signup vc

---
+++ Instagram/SignupViewController.m
@@ -0,0 +1,27 @@
+//
+//  SignupViewController.m
+//  Instagram
+//
+//  Created by Iván Mervich on 8/19/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import "SignupViewController.h"
+
+@interface SignupViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
+@end
+
+@implementation SignupViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+@end
