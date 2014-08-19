df3afe789be9d8b5cfe528a0bb3146568b17751a
Iván Mervich <imervich@gmail.com>
2014-08-19 14:49:05 -0500

    Added class for login screen

---
+++ Instagram/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Instagram
+//
+//  Created by Iván Mervich on 8/19/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ Instagram/LoginViewController.m
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.m
+//  Instagram
+//
+//  Created by Iván Mervich on 8/19/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+@end
