ed303da1add230eec4cb293dd7c58fe41a9a6b69
Iván Mervich <imervich@gmail.com>
2014-08-20 11:02:37 -0500

    Modified User Porofile VC

--- Instagram/UserProfileViewController.m
+++ Instagram/UserProfileViewController.m
@@ -9,41 +9,23 @@
 #import "UserProfileViewController.h"
 
 @interface UserProfileViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *userImageView;
 
 @end
 
 @implementation UserProfileViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+	[self setUserImageViewRoundCorners];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)setUserImageViewRoundCorners
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+	self.userImageView.layer.cornerRadius = self.userImageView.bounds.size.width / 2;
+	self.userImageView.layer.masksToBounds = YES;
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
